b21ebbad38a48863a9553735b9086ee229ae994e
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2026-02-08 08:31:40 -0500

    creacion de funcion para hallar el nombre del producto, lo primero que hacemos es definir el metodo que trabajara con una matriz y un nombre a buscar,, esas son sus herramientas de trabajo,, luego vamos a usar la variable filas para recorrer cada indice  de la matriz, recordemos que tenemos una matriz de matrices, entonces filas se encargara de ser la variable que recorra cada indice de la matriz externa y para eso tenf}dra 2 herramientas para trabajar sobre la matriz, len y range, len le ayudara recorrer y contar cuantos indices hay y range le ayudara a organizarlos en indices, ahora vamos a necesitar otro ciclo, pero este actuara dentro de la variable filas que sabemos son listas la diferencia es que esta ya no solo recorrera la matriz externa en saltos grandes, sino que ira una por una para eso fue que definimos  la variable filas, y para esto usara el ciclo for tambien, y las mismas herramientas que la variable fila, solo que esta se va llamar columnas, para entender mejor esto solo hay que darnos cuenta que la variable columnas no trabaja sobre la matriz sino que en la sintaxis tenemos que poner matriz[filas] para que nuestro len y range trabajen a gusto, ahora como sabemos que nuestra variable columna se va encargar de recorrer todos los productos de nuestra tienda le daremos una herramienta mas sofisticada para que logre su cometido, un condicional if, este le permitira comparar mediante una variable de sintaxis matriz[fila][columna] == nombre a buscar , esta variable nos ayudara a buscar nuestro nombre del producto ya que una vez recorra toda nuestra  matriz el va tener toda la infrmacion gracia al trabajo hecho por la variablee fila y gracias a las herramientas len y range , ahora solo es que le ingresemos el nombre del producto y el sabra exactamente donde se encuentra y nos retornara su fila y su columna, y en caso tal de que ninguno se cumpla pues se devovlvera al ciclo forinicial donde tendremos un return listo para deir producto no encontrado

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -7,3 +7,9 @@ class producto:
 
 almacen= [[producto("aretes",5000, 50), producto("collares", 10000, 20)], [producto("manillas", 12000, 100), producto("anillos", 5000, 150)]]
 
+def buscar_producto(matriz, nombre_a_buscar):
+    for fila in range(len(matriz)):
+        for columna in range(len(matriz[fila])):
+            if matriz[fila][columna].nombre ==nombre_a_buscar:
+                return fila, columna
+    return "producto no encontrado"        
