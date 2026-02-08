8f131070ed9aadbaf65b6621c5ab37f5365eb473
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2026-02-08 07:50:43 -0500

    creacion de clase producto y creacion de matriz 2x2, no olvidemos que hay que hacer 2 filas con 2 productos para que sea 2x2

---
+++ EJERCICIO1.py
@@ -0,0 +1,9 @@
+#repaso de matrices
+class producto:
+    def __init__(self. nombre. precio. cantidad):
+        self.nombre = nombre
+        self.precio = precio 
+        self.cantidad = cantidad
+
+almacen= [[producto("aretes",5000, 50), producto("collares", 10000, 20)], [producto("manillas", 12000, 100), producto("anillos", 5000, 150)]]
+
